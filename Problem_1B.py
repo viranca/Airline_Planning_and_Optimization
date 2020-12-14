0d06a7886b39595debf9c81a1c7d7f862e86317b
viranca <37591728+viranca@users.noreply.github.com>
2020-12-14 23:18:59 +0100

    costs

--- Problem_1B.py
+++ Problem_1B.py
@@ -13,10 +13,10 @@ Airports = ['London', 'Paris', 'Amsterdam',	'Frankfurt', 'Madrid',	'Barcelona', 
             'Rome', 'Dublin', 'Stockholm', 'Lisbon', 'Berlin', 'Helsinki', 'Warsaw', 'Edinburgh', 'Bucharest', 
             'Heraklion', 'Reykjavik',	'Palermo', 'Madeira']
 
-Aircraft = {"1": {"sp": 550, "s": 45,  "TAT": 25, "range": 1500,  "runway": 1400},
-            "2": {"sp": 820, "s": 70,  "TAT": 35, "range": 3300,  "runway": 1600},
-            "3": {"sp": 850, "s": 150, "TAT": 45, "range": 6300,  "runway": 1800},
-            "4": {"sp": 870, "s": 320, "TAT": 60, "range": 12000, "runway": 2600}}
+# Aircraft = {"1": {"sp": 550, "s": 45,  "TAT": 25, "range": 1500,  "runway": 1400},
+#             "2": {"sp": 820, "s": 70,  "TAT": 35, "range": 3300,  "runway": 1600},
+#             "3": {"sp": 850, "s": 150, "TAT": 45, "range": 6300,  "runway": 1800},
+#             "4": {"sp": 870, "s": 320, "TAT": 60, "range": 12000, "runway": 2600}}
 
 
 airports = range(len(Airports))
@@ -34,9 +34,10 @@ y = 0.18          #yield
 demand, distance and yield for every flight leg:
 =============================================================================
 """
-with open('q_placeholder.csv', 'r') as f:
+with open('q_demand.csv', 'r') as f:
     df_demand = pd.read_csv(f, sep=';', header=0)
 q = np.array(df_demand)
+
 
 with open('distance_df.csv', 'r') as f:
     df_distance = pd.read_csv(f, sep=',', header=0)
@@ -50,8 +51,101 @@ for i in range(len(Yield)):
         if i == j:
             Yield[i][j] = 0
             
+"""
+=============================================================================
+Compute a costmatrix for each aircraft type at each flight leg.
+=============================================================================
+"""
+#aircraft 1
+weekly_cost_1 = 15000
+#fixed cost
+C_X_1 = 300
+#time-based cost
+c_T_1 = 750
+V_1 = 550
+C_T_1 = c_T_1 * df_distance / V_1
+#fuel cost
+c_F_1 = 1
+f = 1.42
+C_F_1 = ((c_F_1 * f)/1.5) * df_distance
+C_1 = C_X_1 + C_T_1 + C_F_1 
 
-    
+#aircraft 2
+weekly_cost_2 = 34000
+#fixed cost
+C_X_2 = 600
+#time-based cost
+c_T_2 = 775
+V_2 = 820
+C_T_2 = c_T_2 * df_distance / V_2
+#fuel cost
+c_F_2 = 2
+f = 1.42
+C_F_2 = ((c_F_2 * f)/1.5) * df_distance
+C_2 = C_X_2 + C_T_2 + C_F_2 
+
+#aircraft 3
+weekly_cost_3 = 80000
+#fixed cost
+C_X_3 = 1250
+#time-based cost
+c_T_3 = 1400
+V_3 = 850
+C_T_3 = c_T_3 * df_distance / V_3
+#fuel cost
+c_F_3 = 3.75
+f = 1.42
+C_F_3 = ((c_F_3 * f)/1.5) * df_distance
+C_3 = C_X_3 + C_T_3 + C_F_3 
+
+#aircraft 4
+weekly_cost_4 = 190000
+#fixed cost
+C_X_4 = 2000
+#time-based cost
+c_T_4 = 2800
+V_4 = 870
+C_T_4 = c_T_4 * df_distance / V_4
+#fuel cost
+c_F_4 = 9.0
+f = 1.42
+C_F_4 = ((c_F_4 * f)/1.5) * df_distance
+C_4 = C_X_4 + C_T_4 + C_F_4 
+  
+#accounting economies of scale (departing and arriving from hub)
+C_1 = np.array(C_1)
+for i in range(len(C_1)):
+    for j in range(len(C_1)):
+        if i == 1 and j != 1:
+            C_1[i][j] = 0.7*C_1[i][j]
+        if j == 1:
+            C_1[i][j] = 0.7*C_1[i][j]
+            
+C_2 = np.array(C_2)
+for i in range(len(C_2)):
+    for j in range(len(C_2)):
+        if i == 1 and j != 1:
+            C_2[i][j] = 0.7*C_2[i][j]
+        if j == 1:
+            C_2[i][j] = 0.7*C_2[i][j]
+            
+C_3 = np.array(C_3)            
+for i in range(len(C_3)):
+    for j in range(len(C_3)):
+        if i == 1 and j != 1:
+            C_3[i][j] = 0.7*C_3[i][j]
+        if j == 1:
+            C_3[i][j] = 0.7*C_3[i][j]
+            
+C_4 = np.array(C_4)           
+for i in range(len(C_4)):
+    for j in range(len(C_4)):
+        if i == 1 and j != 1:
+            C_4[i][j] = 0.7*C_4[i][j]
+        if j == 1:
+            C_4[i][j] = 0.7*C_4[i][j]
+
+
 #%%
 """
 =============================================================================
@@ -85,9 +179,6 @@ for i in airports:
 
 m.addConstr(quicksum(quicksum((distance[i][j]/sp+LTO)*z[i,j] for i in airports) for j in airports),
             GRB.LESS_EQUAL, BT*AC) #C4
-
-
-
 
 
 m.update()
